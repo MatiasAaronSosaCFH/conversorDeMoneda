332e5d840572edc79e4bbb7e966625663f82456f
Matias <akucfh@gmail.com>
2023-07-16 00:13:14 -0300

    Conversor v1.03

---
+++ ConversorDeMonedaProject/src/conversordemoneda/ConversorDeMoneda.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conversordemoneda;
+
+import java.util.Scanner;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import static services.MenuService.menuInicio;
+
+
+/**
+ *
+ * @author 54117
+ */
+public class ConversorDeMoneda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        menuInicio();
+        
+    }
+    
+}
