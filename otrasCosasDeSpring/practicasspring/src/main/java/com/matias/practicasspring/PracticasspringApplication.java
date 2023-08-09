e6619ff695367ac04ee1e96b0dca01a800ee8dd7
Matias <akucfh@gmail.com>
2023-08-09 11:15:19 -0300

    ConversoresUnoYDos

---
+++ otrasCosasDeSpring/practicasspring/src/main/java/com/matias/practicasspring/PracticasspringApplication.java
@@ -0,0 +1,13 @@
+package com.matias.practicasspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticasspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticasspringApplication.class, args);
+	}
+
+}
---
+++ otrasCosasDeSpring/practicasspring/src/test/java/com/matias/practicasspring/PracticasspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.practicasspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticasspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
