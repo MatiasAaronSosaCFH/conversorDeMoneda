42bd0fa65bd4c0f2012c423c7290d2affd5ab47e
Matias <akucfh@gmail.com>
2023-10-07 14:27:29 -0300

    conversor-otromas

---
+++ conversor-master/src/main/java/org/conversor/Main.java
@@ -0,0 +1,14 @@
+package org.conversor;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Ventana v = new Ventana();
+        v.setVisible(true);
+
+    }
+}
